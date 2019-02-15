e4b1bfc43525703ad8633a870a8f50a45bca12ff
Luís Carvalho <lapc19@hotmail.com>
2019-02-15 01:25:48 +0000

    Working Clock

--- src/index.js
+++ src/index.js
@@ -6,24 +6,44 @@ import * as serviceWorker from './serviceWorker';
 
 function Segment(props)  {
   
-    const color = props.light ?
-          {fill:'#f00'}:
-          { fill:'#333333'};
+  const color = props.light ?
+        {fill:'#f00'}:
+        {fill:'#d2d2d2'};
+
+  const current_position = props.vertical ?
+        "rotate(-90 " + props.x + " " + props.y + ")":
+        "0";
   
-    const current_position = props.vertical ?
-          "rotate(-90 " + props.x + " " + props.y + ")":
-          "0";
-    
-     return (<rect 
-                x={props.x} 
-                y={props.y} 
-                width="100" 
-                height="25"  
-                transform = {current_position} 
-                style= {color}
-               />
-    );
-  }
+  return (<rect 
+            x={props.x} 
+            y={props.y} 
+            width="100" 
+            height="25"  
+            transform = {current_position} 
+            style= {color}
+            />
+          );
+
+}
+
+function TwoDots(props)  {
+  return (
+    <svg width="60" height="275">  
+      <rect 
+        x="35" 
+        y="75" 
+        width="20" 
+        height="20"   
+      />
+      <rect 
+        x="35" 
+        y="175" 
+        width="20" 
+        height="20"   
+      />
+    </svg>
+  );
+}
   
   class SevenSegmentDisplay extends React.Component {
     constructor(props) {
@@ -38,9 +58,6 @@ function Segment(props)  {
           {x:25, y:125, vertical: true, shift: 1},
           {x:50, y:125, vertical: false, shift: 0},
         ],
-        
-        hexes: [0x7E, 0x30, 0x6D, 0x79, 0x33, 0x5B, 0x5F, 0x70, 0x7F, 0x7B, 0x77, 0x1F, 0x4E, 0x3D, 0x4F, 0x47],
-        index: 0,
       }
     } 
     
@@ -56,29 +73,11 @@ function Segment(props)  {
           />
       );
     }
-    
-    componentDidMount() {
-      this.intervalID = setInterval(
-        () => this.tick(),
-        1000
-      );
-    }
-    componentWillUnmount() {
-      clearInterval(this.intervalID);
-    }
-    tick() {
-      if(this.state.index>14)
-        this.state.index = -1
-      
-      this.setState({
-        index: this.state.index+1
-      });
-    }
-    
+  
     render() {
       var segments = Array(7);
       for(var i=0; i < this.state.segmentsAttributes.length; i++) {
-        segments[i] = this.renderSegment(i, this.state.hexes[this.state.index]);
+        segments[i] = this.renderSegment(i, this.props.hex);
       }  
       return (<svg width="175" height="275"> {segments} </svg>)
     }
@@ -100,8 +99,119 @@ function Segment(props)  {
        
     */
     
+
+    constructor(props) {
+      super(props);
+      this.state = {
+          hexes: [0x7E, 0x30, 0x6D, 0x79, 0x33, 0x5B, 0x5F, 0x70, 0x7F, 0x7B, 0x77, 0x1F, 0x4E, 0x3D, 0x4F, 0x47],
+
+          second_1: {min:0, max:5},
+          second_2: {min:0, max:9},
+          second_1_index: 5,
+          second_2_index: 9,
+
+          minute_1: {min:0, max:5},
+          minute_2: {min:0, max:9},
+          minute_1_index: 5,
+          minute_2_index: 9,
+
+          hour_1: {min:0, max:2},
+          hour_2: {min:0, max:9},
+          hour_1_index: 0,
+          hour_2_index: 9,
+
+        }
+    } 
+
+    componentDidMount() {
+      this.intervalID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+    componentWillUnmount() {
+      clearInterval(this.intervalID);
+    }
+    tick() {
+      if(this.state.second_2_index >= this.state.second_2.max) {
+        this.setState({
+          second_2_index: 0
+        });
+
+        if(this.state.second_1_index >= this.state.second_1.max) {
+          this.setState({
+            second_1_index: 0,
+          });
+
+          if(this.state.minute_2_index >= this.state.minute_2.max) {
+            this.setState({
+              minute_2_index: 0,
+            });
+
+            if(this.state.minute_1_index >= this.state.minute_1.max) {
+              this.setState({
+                minute_1_index: 0,
+              });
+
+              if((this.state.hour_1_index == 2 && this.state.hour_2_index >= 3) || (this.state.hour_2_index >= this.state.hour_2.max)) {
+                this.setState({
+                  hour_2_index: 0,
+                });
+
+                if(this.state.hour_1_index >= this.state.hour_1.max) {
+                  this.setState({
+                    hour_1_index: 0,
+                  });
+                } else {
+                  this.setState({
+                    hour_1_index: this.state.hour_1_index+1,
+                  });
+                }
+
+              } else {
+                this.setState({
+                  hour_2_index: this.state.hour_2_index+1,
+                });
+              }
+
+            } else {
+              this.setState({
+                minute_1_index: this.state.minute_1_index+1,
+              });
+            }
+
+          } else {
+            
+            this.setState({
+              minute_2_index: this.state.minute_2_index+1,
+            });
+          }
+
+        } else {
+          this.setState({
+            second_1_index: this.state.second_1_index+1,
+          });
+        }
+
+      } else {
+        this.setState({
+          second_2_index: this.state.second_2_index+1
+        });
+      }
+      console.log("tick!");
+    }
+
     render() {
-      return <div> <SevenSegmentDisplay /> <SevenSegmentDisplay /> </div>
+      return <div>
+        <SevenSegmentDisplay hex={this.state.hexes[this.state.hour_1_index]}/>
+        <SevenSegmentDisplay hex={this.state.hexes[this.state.hour_2_index]}/>
+        <TwoDots></TwoDots> 
+        <SevenSegmentDisplay hex={this.state.hexes[this.state.minute_1_index]}/> 
+        <SevenSegmentDisplay hex={this.state.hexes[this.state.minute_2_index]}/>
+        <TwoDots></TwoDots>  
+        <SevenSegmentDisplay hex={this.state.hexes[this.state.second_1_index]}/> 
+        <SevenSegmentDisplay hex={this.state.hexes[this.state.second_2_index]}/> 
+        </div>
     }
   }
   
